5dcd90ef5fb485ffcf1d6fa8722b39fcb0c51ea1
Tom Nijland <tomnijland@planet.nl>
2023-07-21 14:10:28 +0200

    1e push Python

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/tweedeendpoint")
+def hello_world1():
+    return "Iets heel anders teruggeven"
+
+@app.route("/derde/<onsgegeven>")
+def hello_world3(onsgegeven):
+    return "Dit is ons gegeven"+onsgegeven
